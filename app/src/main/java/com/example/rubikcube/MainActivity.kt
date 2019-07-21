0047304ae3c16c7d2489ecaf98eb2c69c0b3b517
alex_bo <bozhenko.oleksey@gmail.com>
2019-07-21 13:43:38 +0300

    add layout with buttons to main activity

--- app/src/main/java/com/example/rubikcube/MainActivity.kt
+++ app/src/main/java/com/example/rubikcube/MainActivity.kt
@@ -3,25 +3,24 @@ package com.example.rubikcube
 import android.app.Activity
 import android.content.res.AssetManager
 import android.os.Bundle
-import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams.FILL_PARENT
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.widget.RelativeLayout
-import kotlinx.android.synthetic.main.activity_main.view.*
+import android.widget.LinearLayout
+
 
 class MainActivity : Activity() {
     init {
         System.loadLibrary("native-lib")
     }
 
-    internal lateinit var mView: GLESView
+    private lateinit var mView: GLESView
 
     override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
         initAssetManager(getAssets())
-        mView = GLESView(application)
         setContentView(R.layout.activity_main)
-        addContentView(mView, RelativeLayout.LayoutParams(300, 300))
+        mView = GLESView(application)
+
+        val layoutMain = findViewById(R.id.linear_layout) as LinearLayout
+        layoutMain.addView(mView)
     }
 
     override fun onPause() {
